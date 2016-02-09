414364efabdbd1a3b24e57d6466a23cddd47ed70
richardtallent <richard@tallent.us>
2016-02-09 09:26:13 -0800

    Initial commit

---
+++ src/RT.PerformanceTimer.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+namespace RT {
+
+	/// <summary>
+	/// http://msdn.microsoft.com/en-us/library/ms182161.aspx
+	/// </summary>
+	[System.Security.SuppressUnmanagedCodeSecurity] internal static class SafeNativeMethods {
+	
+		/// <summary>
+		/// Provides a much higher-resolution timer than Environment.TickCount
+		/// </summary>
+		[System.Runtime.InteropServices.DllImport("Kernel32.dll")] public static extern bool QueryPerformanceCounter(out long perfcount);		
+
+		/// <summary>
+		/// Returns the number of QueryPerformanceCounter() values per second
+		/// </summary>
+		[System.Runtime.InteropServices.DllImport("Kernel32.dll")] public static extern bool QueryPerformanceFrequency(out long freq);
+
+	}
+
+	/// <summary>
+	/// This timer class can be used to test the performance of critical code blocks with
+	/// low overhead and high precision. Sets of PerformanceTimers should be implemented
+	/// as properties of a class inheriting from PerformanceTimers.
+	/// 
+	/// Note that the Start() / Finish() methods are not thread-safe -- timer instances
+	/// shared between threads should only be called using the Time() method so overlapping
+	/// timer calls won't impact one another.
+	/// </summary>
+	public sealed class PerformanceTimer {
+		
+		/// <summary>
+		/// Preserves the start time when Start() is called, and is used only by Finish().
+		/// </summary>
+		private long _lastStartTime;
+		private long _lastFinishTime;
+
+		/// <summary>
+		/// Optional, allows this to be easily emitted to a database, web page, etc.
+		/// </summary>
+		public string Name			{ get; set; }
+
+		/// <summary>
+		/// Total run time (in ticks) of this timer instance so far (using Start() or Time()).
+		/// </summary>
+		public long TotalDuration	{ get; private set; }
+
+		/// <summary>
+		/// Total number of times this timer has been *started* so far (using Start() or Time()).
+		/// </summary>
+		public int Count			{ get; private set; }
+
+		// Utility read-only properties for display of statistics
+		public long AverageDuration		{ get { return Count==0 ? 0 : TotalDuration / Count; } }
+		public long AverageDurationMS	{ get { return Count==0 ? 0 : TotalDurationMS / Count; } }
+		public long TotalDurationMS		{ get { return ConvertToMS(TotalDuration); } }
+
+		public long LastDurationMS		{ get { return ConvertToMS(_lastFinishTime - _lastStartTime); } }
+
+		/* *************************************************************************** */
+		// Constructor and Public methods.
+		/* *************************************************************************** */
+
+		public PerformanceTimer(string name = null) {
+			this.Name = name;
+		}
+
+		/// <summary>
+		/// Starts the timer and increments the counter. 
+		/// </summary>
+		public void Start() {
+			if( (_lastStartTime > 0) && (_lastFinishTime == _lastStartTime) )
+			{
+				// Timer did not finish, was called again, probably recursively.
+				// Call Finish() so the timing is cumulative, and don't increment
+				// the count.
+				Finish();
+				_lastStartTime = _lastFinishTime;		// Avoid another system call, make timing continuous
+			} else {
+				Count++;
+				_lastStartTime = GetCurrentTimerValue();
+			}
+			_lastFinishTime = _lastStartTime;
+		}
+
+		/// <summary>
+		/// Adds the number of ticks since the most recent Start() call.
+		/// </summary>
+		public void Finish() {
+			_lastFinishTime = GetCurrentTimerValue();
+			TotalDuration += _lastFinishTime - _lastStartTime;
+		}
+
+		/// <summary>
+		/// Alternative method to separate Start/Finish calls. Use only with complex blocks of code with
+		/// few iterations, otherwise the overhead of passing the delegate action may be significant.
+		/// For performance, this inlines its own version of Start/Finish that doesn't rely on _lastStartTime / _lastFinishTime.
+		/// Returns itself to allow for fluid calls.
+		/// Usage:
+		///		myTimer.Time( () => { Dostuff; } );
+		/// </summary>
+		public PerformanceTimer Time(Action action) {
+			Count++;
+			var start = GetCurrentTimerValue();
+			action();
+			TotalDuration += GetCurrentTimerValue() - start;
+			return this;
+		}
+
+		/// <summary>
+		/// Resets everything but the name.
+		/// </summary>
+		public void Reset() {
+			_lastStartTime = 0;
+			_lastFinishTime = 0;
+			TotalDuration = 0;
+			Count = 0;
+		}
+
+		/* *************************************************************************** */
+		// Private methods
+		/* *************************************************************************** */
+
+		private static long GetCurrentTimerValue() {
+			long result;
+			SafeNativeMethods.QueryPerformanceCounter(out result);
+			return result;
+		}
+
+		private static long ConvertToMS(long duration) {
+			long ticksPerSecond;
+			SafeNativeMethods.QueryPerformanceFrequency(out ticksPerSecond);
+			long ticksPerMS = ticksPerSecond / 1000L;
+			return duration / ticksPerMS;
+		}
+
+	}
+
+}
---
+++ src/RT.PerformanceTimers.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace RT {
+
+	/// <summary>
+	/// Groups of performance timers should inherit from this, add their own Timer fields (ideally read-only)
+	/// and reference them directly.
+	/// 
+	/// This was previously implemented as a Dictionary, but the hash code lookups were far too slow to use
+	/// this to time critical code in loops. Enumerating the properties to emit the result is slower than
+	/// the dictionary (as it uses reflection), but since this is only called once, when all of the timing
+	/// is complete (say, at the end of a page render), the difference is negligible.
+	/// 
+	/// Note that a PerformanceTimers subclass can also have fields that are instances of *other* subclasses
+	/// of PerformanceTimers -- the reflection calls will loop through them when emitting the list of timers.
+	/// This allows a long list of timers to be grouped, and allows some composition of groups of timers to
+	/// overcome C# limitations of single inheritance.
+	/// </summary>
+	public class PerformanceTimers {
+
+		/// <summary>
+		/// Intended to be used when this instance is a property of another PerformanceTimers instance,
+		/// allows all of the timers to be emitted with a common name prefix.
+		/// </summary>
+		public string NamePrefix { get; set; } = string.Empty;
+
+		public void Reset() {
+			foreach(var timer in Timers) timer.Reset();
+		}
+
+		/// <summary>
+		/// Returns a list of all PerformanceTimer fields that are either members of this class
+		/// or are fields of PerformanceTimer-derived fields. This recurses, allowing PerformanceTimers
+		/// subclasses to have PerformanceTimers fields, which can then also have PerformanceTimers
+		/// fields, etc. Implemented as IReadOnlyCollection<> because AFAIK C# doesn't currently 
+		/// support multiple yield return statements to allow for an IEnumerable<> implementation.
+		/// </summary>
+		public IReadOnlyCollection<PerformanceTimer> Timers {
+			get {
+				// Initialize the list with the PerformanceTimer fields
+				var timers = new List<PerformanceTimer>(MemberTimers);
+				// Loop through PerformanceTimers-derived fields to add additional timers from them.
+				var listsOfTimers = from timerGroup in MemberTimerGroups select timerGroup.Timers;
+				foreach(var otherTimerList in listsOfTimers) {
+					timers.AddRange(otherTimerList);
+				}
+				// Return a collection that the caller can't modify.
+				return timers.AsReadOnly();
+			}
+		}
+
+		/// <summary>
+		/// Private only, returns the fields of this class that are of type PerformanceTimer
+		/// </summary>
+		private IEnumerable<PerformanceTimer> MemberTimers {
+			get {
+				return this.GetType().GetFields()
+					.Where(fieldinfo => fieldinfo.FieldType == typeof(PerformanceTimer))
+					.Select(fieldinfo => (PerformanceTimer)fieldinfo.GetValue(this));
+			}
+		}
+
+		/// <summary>
+		/// Private only, returns the fields of this class that are *subclasses* of type PerformanceTimers.
+		/// </summary>
+		private IEnumerable<PerformanceTimers> MemberTimerGroups {
+			get {
+				return this.GetType().GetFields()
+					.Where(fieldinfo => typeof(PerformanceTimers).IsAssignableFrom(fieldinfo.FieldType))
+					.Select(fieldinfo => (PerformanceTimers)fieldinfo.GetValue(this));
+			}
+		}
+
+	}
+
+}
